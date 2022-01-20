1478eea82c82a3f0c6ae2700a655b3e9a99c40fe
Robert Sundström <robert.sundstrom@outlook.com>
2022-01-20 23:39:10 +0100

    Refactor

--- Server/Worker/Consumers/DoSomethingConsumer.cs
+++ Server/Worker/Consumers/DoSomethingConsumer.cs
@@ -4,17 +4,26 @@ using Contracts;
 
 using MassTransit;
 
+using MediatR;
+
+using Worker.Application;
+
 namespace Worker.Consumers;
 
 public class DoSomethingConsumer : IConsumer<DoSomething>
 {
+    private readonly IMediator _mediator;
+
+    public DoSomethingConsumer(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     public async Task Consume(ConsumeContext<DoSomething> context)
     {
         var message = context.Message;
 
-        await Task.Delay(Random.Shared.Next(5000));
-
-        var result = message.LHS + message.RHS;
+        var result = await _mediator.Send(new DoSomethingCommand(message.LHS, message.RHS));
 
         await context.Publish(new DoSomethingResponse($"The result is: {result}"));
     }
