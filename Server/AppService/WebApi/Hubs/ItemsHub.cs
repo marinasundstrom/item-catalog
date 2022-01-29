5190a690603f338cd7ecca1e5b8cb80dc251a782
Robert Sundström <robert.sundstrom@outlook.com>
2022-01-29 19:46:17 +0100

    Add auth to SignalR hubs

--- Server/AppService/WebApi/Hubs/ItemsHub.cs
+++ Server/AppService/WebApi/Hubs/ItemsHub.cs
@@ -2,11 +2,16 @@
 
 using Catalog.Application.Common.Interfaces;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Catalog.WebApi.Hubs;
 
+[Authorize]
 public class ItemsHub : Hub<IItemsClient>
 {
-
+    public override Task OnConnectedAsync()
+    {
+        return base.OnConnectedAsync();
+    }
 }
