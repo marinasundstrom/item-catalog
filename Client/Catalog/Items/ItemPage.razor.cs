3000300604b39f68ea02a35a2757637228a58f9e
Robert Sundström <robert.sundstrom@outlook.com>
2022-03-17 18:24:17 +0100

    Comment Section

--- Client/Catalog/Items/ItemPage.razor.cs
+++ Client/Catalog/Items/ItemPage.razor.cs
@@ -8,7 +8,6 @@ namespace Catalog.Items
 {
     public partial class ItemPage
     {
-        MudTable<CommentDto> table;
         ItemDto? item;
 
         bool isLoading = false;
@@ -67,54 +66,6 @@ namespace Catalog.Items
         async void OnLocationChanged(object sender, LocationChangedEventArgs ev)
         {
             await LoadAsync();
-            await table.ReloadServerData();
-        }
-
-        private async Task<TableData<CommentDto>> ServerReload(TableState state)
-        {
-            try
-            { 
-                var results = await ItemsClient.GetCommentsAsync(Id, state.Page, state.PageSize, state.SortLabel, state.SortDirection == MudBlazor.SortDirection.Ascending ? Catalog.Client.SortDirection.Asc : Catalog.Client.SortDirection.Desc);
-                return new TableData<CommentDto>()
-                {
-                    TotalItems = results.TotalCount,
-                    Items = results.Items
-                };
-
-            }
-            catch (AccessTokenNotAvailableException exception)
-            {
-                exception.Redirect();
-            }
-
-            return null!;
-        }
-
-        private async Task OpenDialog(CommentDto? comment)
-        {
-            var parameters = new DialogParameters();
-            parameters.Add(nameof(CommentDialog.ItemId), Id);
-            parameters.Add(nameof(CommentDialog.CommentId), comment?.Id);
-
-            var dialogReference = DialogService.Show<CommentDialog>(comment is not null ? "Update comment" : "Write a comment", parameters);
-            var result = await dialogReference.Result;
-            var model = (CommentDialog.FormModel)result.Data;
-
-            if (result.Cancelled)
-                return;
-
-            await table.ReloadServerData();
-        }
-
-        private async Task DeleteComment(CommentDto comment)
-        {
-            var result = await DialogService.ShowMessageBox($"Delete comment?", "Are you sure?", "Yes", "No");
-            if (result.GetValueOrDefault())
-            {
-                await ItemsClient.DeleteCommentAsync(Id, comment.Id);
-
-                await table.ReloadServerData();
-            }
         }
 
         public void Dispose()
